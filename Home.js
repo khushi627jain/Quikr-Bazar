9663108710ca06f696bd883b75204568a1d018bd
Khushi627jain <deepshikha2764@gmail.com>
2023-04-15 11:43:15 +0530

    project complete

--- Home.js
+++ Home.js
@@ -4,59 +4,7 @@
 
     // fetching data from API and Showing on page
 
-    async function getData(url){
-        try{
-            let res = await fetch(url);
-            let data = await res.json();
-            
-            return data;
-        }
-        catch(err){
-            console.log(err);
-        }
-    }
-    function appendData(data){
-        console.log(data);
-
-        let Head = document.createElement("h2");
-        Head = data.name;
-        showCityNmae.append(Head);
-
-        let temp = document.createElement("p");
-       temp = data.main.temp;
-       showTemp.append( temp);
-       let FeelsTemp = document.createElement("p");
-       FeelsTemp = data.main.feels_like;
-       feels.append(FeelsTemp);
-
-       let showHumidity = document.createElement("p");
-            showHumidity = data.humidity;
-            appendMain.append(showHumidity);
-            
-
-       data.weather.forEach((i) =>  {
-        console.log(data.weather);
-            // let div = document.createElement("div");
-            let p = document.createElement("p");
-           
-            p.textContent = i.description;
-           
-            
-            feels.append(p);
-
-        });
-        
-    }
-    
-    let response = getData(
-            `http://localhost:3000/posts`
-         );
-
-    response.then((res) => {
-        appendData(res);
-    }).catch((err)=> {
-        console.log(err);
-    });
+  
 
     // navbar js ==========
     let userData=JSON.parse(localStorage.getItem("userData"))||[];
@@ -78,4 +26,86 @@
     // for search 
 
     
-const searching = document.getElementById("search").value ;
+
+
+let timerId;
+function debounce(func,time){
+    if(timerId){
+        clearTimeout(timerId);
+    }
+    timerId=setTimeout(func,time)
+   
+}
+async function main(){
+    let data =await fetchMovie();
+    if(!data){
+        return false;
+    }
+    displayData(data);
+}
+
+async function fetchMovie(){
+    try{
+        const searching = document.getElementById("search").value ;
+   console.log(searching)
+        let url=`https://fragile-zipper-ox.cyclic.app/product?subCategory=${searching}`;
+    let data =await fetch(url)
+    data=await data.json();
+
+   
+    console.log(data)
+    
+    return data
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+function displayData(data){
+    let dataShow=document.getElementById("showMovies")
+    dataShow.style.display="block"
+    document.getElementById("showMovies").textContent="";
+    let btn=document.createElement("h3")
+btn.textContent="Clear";
+btn.style.color="#008bcf"
+btn.addEventListener("click",function(){
+    // document.getElementById("search").textContent=""
+    dataShow.style.display="none"
+    document.getElementById("search").value=""
+
+})
+document.getElementById("showMovies").append(btn)
+data.map(function(ele){
+    console.log("hey")
+let movieCard=document.createElement("p");
+movieCard.textContent=ele.title;
+
+// show movie detail
+movieCard.addEventListener("click",showAllDetails)
+function showAllDetails(){
+    let arr=[]
+
+  let allDetails={
+    title:ele.title,
+    price:ele.price,
+    mainCategory:ele.mainCategory,
+    subCategory:ele.subCategory,
+    condition:ele.condition,
+    date:ele.data,
+    adid:ele.adid,
+    sellerType:ele.sellerType,
+    description:ele.description,
+    url:ele.url,
+  }
+  arr.push(allDetails)
+  localStorage.setItem("allProductDetails",JSON.stringify(arr))
+  location.href="kidsDetails.html"
+ 
+}
+//=============
+document.getElementById("showMovies").append(movieCard)
+})
+
+}
+
